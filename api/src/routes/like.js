d5395a16e32d27968127e953258f32104614af64
Robmargar <roberto.mar.garc@gmail.com>
2024-08-24 12:02:49 -0600

    se optimisa el funcionamiento de poner y quitar like

--- api/src/routes/like.js
+++ api/src/routes/like.js
@@ -3,26 +3,40 @@ const router = Router();
 const { User, Videogame } = require("../db.js");
 const { where } = require("sequelize");
 
-// ------ Poner o quitar like de un videojuego ------
+async function encontrarUsuario(userId) {
+  const usuarioVid = await User.findOne({
+    attributes: ["name", "id"],
+    where: {
+      id: userId,
+    },
+    include: [
+      {
+        model: Videogame,
+        attributes: ["name", "id"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  });
+  return usuarioVid;
+}
+// ------ Obtener listado de videojuegos likeados ------
+router.get("/", async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const usuarioVid = await encontrarUsuario(userId);
+    res.status(200).json(usuarioVid.videogames);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+// ------ Poner o quitar like de un videojuego ------
 router.post("/", async (req, res) => {
   const { userId, videogameId } = req.body;
   try {
-    const usuarioVid = await User.findOne({
-      attributes: ["name", "id"],
-      where: {
-        id: userId,
-      },
-      include: [
-        {
-          model: Videogame,
-          attributes: ["name", "id"],
-          through: {
-            attributes: [],
-          },
-        },
-      ],
-    });
+    const usuarioVid = await encontrarUsuario(userId);
     const found = usuarioVid.videogames.find((e) => e.id === videogameId);
     found
       ? usuarioVid.removeVideogames(videogameId)
